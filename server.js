f17b4e7d185526523e5ce81fc477de8f7c50a806
Anthony Pantekoek <137728+jfca@users.noreply.github.com>
2020-07-28 16:29:39 -0400

    Add Cities integration and unit tests

--- server.js
+++ server.js
@@ -22,4 +22,6 @@ app.use('/api/users', require('./routes/users'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server  = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = server;
