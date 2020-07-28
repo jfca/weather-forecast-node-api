61d28d56374bbf2818644463fc655ffe2d5d1711
Anthony Pantekoek <137728+jfca@users.noreply.github.com>
2020-07-28 16:29:39 -0400

    Updated unit tests for cities.spec.js

--- test/routes/cities.spec.js
+++ test/routes/cities.spec.js
@@ -1,16 +1,18 @@
 const sinon = require('sinon');
 const chai = require('chai');
+const expect = require('chai').expect;
+const should = require('chai').should;
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
+
 const City = require('../../models/City');
 const server = require('../../server');
-chai.should();
 
 chai.use(chaiHttp);
 
 describe('Weather Forecast Cities API', function () {
 
-    describe('Integration tests', function () {
+    describe.skip('Integration tests', function () {
 
         describe('GET /api/cities/count', function () {
             it('should return total number of cities', (done) => {
@@ -37,7 +39,39 @@ describe('Weather Forecast Cities API', function () {
     describe('Unit tests', function () {
 
         describe('GET /api/cities/count', function () {
-            it('should return total number of cities', function (done) {
+            it('should be invalid of name is empty', function (done) {
+                const c = new City();
+
+                c.validate(function (err) {
+                    expect(err.errors.name).to.exist;
+                    done();
+                })
+            });
+            it('should be invalid of country is empty', function (done) {
+                const c = new City();
+
+                c.validate(function (err) {
+                    expect(err.errors.country).to.exist;
+                    done();
+                })
+            });
+            it('should be invalid of coord.lon is empty', function (done) {
+                const c = new City();
+
+                c.validate(function (err) {
+                    expect(err.errors['coord.lon']).to.exist;
+                    done();
+                })
+            });
+            it('should be invalid of coord.lat is empty', function (done) {
+                const c = new City();
+
+                c.validate(function (err) {
+                    expect(err.errors['coord.lat']).to.exist;
+                    done();
+                })
+            });
+            it.skip('should return total number of cities', function (done) {
                 const countDocuments = sinon.stub(City, 'countDocuments');
                 chai.request(server)
                     .get('/api/cities/count')
